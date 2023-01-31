110c334644755dc1fb929d3323dcbbb50e1d01b1
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-01-31 19:36:14 -0300

    Update digitoverificadorcpf.py
    digito 31012023

--- digitoverificadorcpf.py
+++ digitoverificadorcpf.py
@@ -1,30 +1,10 @@
-'''
-1	4	5	3	8	2	2	0	6
-X	X	X	X	X	X	X	X	X
-10	9	8	7	6	5	4	3	2
-10	36	40	21	48	10	8	0	12
-10 + 36 + 40 + 21 + 48 + 10 + 8 + 0 + 12 = 185
-Com esse resultado em mãos, vamos dividí-lo por 11, mas o importante para nós não é resultado, mas sim o módulo (resto) da divisão.
 
-185 % 11 = 9
-O resto da divisão é 9. Agora para calcular o dígito verificador vamos subtrair este resto do número 11:
-
-11 - 9 = 2
-Como o resultado da da subtração foi 2, o primeiro dígito verificador é igual a 2. Caso o resultado dessa divisão for 10 ou maior, o penúltimo dígito verificador será o 0.
-1	4	5	3	8	2	2	0	6	2
-X	X	X	X	X	X	X	X	X	X
-11	10	9	8	7	6	5	4	3	2
-11	40	45	24	56	12	10	0	18	4
-11 + 40 + 45 + 24 + 56 + 12 + 10 + 0 + 18 + 4 = 220
-Novamente vamos efetuar a divisão por 11 usando o módulo:
-
-220 % 11 = 0
-E vamos fazer a subtração:
-
-11 - 0 = 11
-Como o valor é igual ou maior que 10, o último dígito é 0.
-'''
 from time import sleep 
+
+'''Outra regra muito importante é que CPFs com números iguais como: 111.111.111-11 , 222.222.222-22 
+entre outros, são CPFs válidos pelo algoritmo mas
+não existem no registro oficial. Assim esse tipo de CPF não pode ser usado'
+'''
 def continua():
     while True:
         try:
@@ -51,39 +31,45 @@ def leiaStr(msg):
             print('\033[31mdigite opção valida S ou N: \033[m')
     else:
         if len(n) != 11 or not n.isnumeric():
-             print(f'vc digitou {n}')
              print('\033[31mdigite número valido\033[m')
         else:
              return n
     
-verifica=leiaStr('Entre com número inteiro:.')
-nove=0
-calulaprimeiro=0
-calculasegundo=0
+while True:    
+ verifica=leiaStr('Digite o número sem ponto e sem traço:') # obs fiz a lógica assimdigitando números direto, só mudar a lógica  
+ verifica1=verifica[9:11]
+ nove=0
+ calulaprimeiro=0
+ calculasegundo=0
 
-soma=0
-for x in range(10,1,-1): #de 10 até 2
+ soma=0
+ for x in range(10,1,-1): #de 10 até 2
    soma=soma + int(verifica[nove]) *  x
    nove+=1
 
-calculaprimeiro=11-(soma % 11)
-if calculaprimeiro >=10:
-    calclaprimeiro=0
-digitos=''
-soma=0
-nove=0
-verifica = verifica[0:9]+str(calculaprimeiro)
+ calculaprimeiro=11-(soma % 11)
+ if calculaprimeiro >=10:
+     calclaprimeiro=0
+ 
+ soma=0
+ nove=0
+ verifica = verifica[0:9]+str(calculaprimeiro)
 
-for x in range(11,1,-1): #de 11 até 2
+ for x in range(11,1,-1): #de 11 até 2
    soma=soma + int(verifica[nove]) *  x
    nove+=1
-calculasegundo=11-(soma % 11)
-if calculasegundo >=10:
+ calculasegundo=11-(soma % 11)
+ if calculasegundo >=10:
     calculasegundo=0
-digitos=str(calculaprimeiro)+str(calculasegundo)
-verifica = verifica+str(calculasegundo)
-if digitos==verifica[9:11]:
-  print(f'digito verificador calculado é valido' )      
-verifica = verifica[0:9]+'-'+str(calculaprimeiro)+str(calculasegundo)
-
-print(f'digito verificador calculado é = {digitos}' )
+ 
+ verifica = verifica+str(calculasegundo)
+ if verifica1==verifica[9:11]:
+   verifica=verifica[0:3]+'.'+verifica[3:6]+'.'+verifica[6:9]+'-'+verifica[9:11]
+   print(f'digito verificador calculado é válido: {verifica} ' )      
+   verifica = verifica[0:9]+'-'+str(calculaprimeiro)+str(calculasegundo)
+   print(f'digito verificador calculado é = {str(calculaprimeiro)+str(calculasegundo)}' )
+ else:
+   print('digito verificador calculado é inválido')    
+ resp=continua()
+ if resp=='N':
+   break
