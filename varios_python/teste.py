c0ef78cb942f2e61543db0cb392b58e2e384f9a1
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-03-05 15:03:18 -0300

    alterado teste

--- varios_python/teste.py
+++ varios_python/teste.py
@@ -28,3 +28,55 @@ def numeros(*num):
 
 x= numeros(1,4,10,20)
 print (x)
+
+# trbalhando com geradores
+'''def a():
+   yield 20
+
+for x in a():
+   print(x)
+
+
+def b():
+   n=0
+   while True:
+      n+=1
+      yield n
+
+for i in b():
+   print(i)
+'''
+def evens(limit=10):
+   n=0
+   while True:
+      n+=2
+      if n>limit:
+        return
+      yield n
+
+def odds(limit=10):
+   n=1
+   while True:
+      n+=2
+      if n>limit:
+        return
+      yield n
+
+def chain(g1, g2):
+   '''for i in g1:
+      yield i 
+   for i in g2:
+       yield i    dois for substituidos pelas linhas abaixo'''
+   yield from g1
+   yield from g2
+#for i in chain(evens(), odds()):
+#    print(i)
+
+a= evens()
+print(next(a))
+print(next(a))
+print(next(a))
+ 
+  
+   
+
