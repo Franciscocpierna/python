907d008e84775098334f181f95b74aba58f07a8f
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-04-07 14:26:39 -0300

    bootle com arquivo html

--- varios_python/usandoframeworkbootle.py
+++ varios_python/usandoframeworkbootle.py
@@ -2,6 +2,7 @@ from bottle import route, run, template
 @route('/')
 @route('/hello/<name>')
 def hello(name='Stranger'):
-   return template('Hello {{name}}, how are you?', name=name)
+   #return template('Hello {{name}}, how are you?', name=name)
+   return template("index")
 
 run(host='localhost', port=8080, debug=True)
