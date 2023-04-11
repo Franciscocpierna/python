caa0abc8daf7ed85906d349977e61f00c7780d88
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2023-04-11 18:07:45 -0300

    atualizaçao bootle

--- varios_python/usandoframeworkbootle.py
+++ varios_python/usandoframeworkbootle.py
@@ -1,8 +1,11 @@
 from bottle import route, run, template, static_file
 
+#@route('/static/<filename>') # rota achar arquivos estaticos
 @route('/static/<filename>')
 def server_static(filename):
     return static_file(filename, root='./views/static')
+    
+    
 
 @route('/')
 @route('/hello/<name>')
@@ -12,3 +15,6 @@ def hello(name='Stranger'):
 
 run(host='localhost', port=8080, debug=True)
 
+#https://bottlepy.org/docs/dev/tutorial.html#generating-content
+#http://localhost:8080/
+#http://localhost:8080/hello/ola
